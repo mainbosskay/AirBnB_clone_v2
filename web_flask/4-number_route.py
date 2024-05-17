6c7bc733f64896412ceb5a083ca6b57056865cbe
mainbosskay <dadakehinde80@yahoo.com>
2024-05-17 12:46:44 -0700

    Python program that writes a a script that starts a Flask web application displaying “n is a number”

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Python script that starts a Flask web application"""
+from flask import Flask
+
+
+app = Flask(__name__)
+"""Flask application instance is established"""
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def home():
+    """This is the home page message"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb_page():
+    """This is the HBNB page message"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_page(text):
+    """C page message displying by value <text>"""
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route("/python/<text>")
+@app.route("/python", defaults={"text": "is cool"})
+def python_page(text):
+    """Python page displaying the value of <text>"""
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+@app.route("/number/<int:n>")
+def number_page(n):
+        """"Number page displaying n if it is integer"""
+        return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
